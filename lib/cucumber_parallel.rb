a9b64a328d3c9cd255c2238274fe45e11394c568
Juan Jose Ortega <jjortega@MacBook-Pro-de-Juan.local>
2016-11-21 16:03:55 +0100

    Upload split of parallel a non parallel options

--- lib/cucumber_parallel.rb
+++ lib/cucumber_parallel.rb
@@ -4,15 +4,13 @@ require "cucumber_parallel/nodes/node_provider"
 require "cucumber_parallel/cucumber_thread_handler"
 require "cucumber_parallel/feature/feature_spliter"
 
-require "byebug"
-
 module CucumberParallel
   class Mode
-    def self.split_features(url, feature_path, **cucumber_configuration) #TODO: Change URL for NodeProviderConfiguration
-      node_provider = NodeProvider.new(url).nodes_with_lock
+    def self.split_features(cucumber_parallel_options, cucumber_configuration) #TODO: Change URL for NodeProviderConfiguration
+      node_provider = NodeProvider.new(cucumber_parallel_options[:url]).nodes_with_lock
       cucumber_thread_handler = CucumberThreadHandler.new(node_provider)
-      _feaures_path = FeatureSpliter.new(feature_path).split
-      cucumber_thread_handler.run(_feaures_path.to_a)
+      feaures_path = FeatureSpliter.new(cucumber_parallel_options[:feature_path]).split
+      cucumber_thread_handler.run(feaures_path.to_a,cucumber_configuration)
     end
   end
 end
--- lib/cucumber_parallel/cucumber_executor.rb
+++ lib/cucumber_parallel/cucumber_executor.rb
@@ -5,15 +5,14 @@ module CucumberParallel
     include Celluloid
     attr_reader :result
 
-    def run(node, feature_path, *cucumber_configuration)
+    def run(node, feature_path, cucumber_configuration)
       node.lock
       randomNumber = Random.rand(1000)
       deviceName = node.name
-      #cmd = "DEVICE_NAME='#{deviceName}' cucumber -p android #{feature_path} --format pretty --format html --out results/cucumber#{randomNumber}.html --format json --out results/cucumber#{randomNumber}.json"
-      cmd = "DEVICE_NAME='#{deviceName}' cucumber -p android #{feature_path} --format pretty --format html --out results/cucumber#{randomNumber}.html --format json --out results/cucumber#{randomNumber}.json"
+      cucumber_configuration = cucumber_configuration.join(' ')
+      cmd = "DEVICE_NAME='#{deviceName}' cucumber #{cucumber_configuration} #{feature_path} --format pretty --format html --out results/cucumber#{randomNumber}.html --format json --out results/cucumber#{randomNumber}.json"
       p "Running: #{cmd}"
       @result = %x(#{cmd})
-      sleep(10)
       node.release
       "Command End: #{cmd}"
     end
