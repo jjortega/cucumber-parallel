c0e161b21e69fdd69c83df2eed75e270f18aee84
Juan Jose Ortega <jjortega@MacBook-Pro-de-Juan.local>
2016-11-24 17:03:32 +0100

    Handler exception and release the node

--- lib/cucumber_parallel/cucumber_executor.rb
+++ lib/cucumber_parallel/cucumber_executor.rb
@@ -5,8 +5,14 @@ module CucumberParallel
   class CucumberExecutor
     include Celluloid
     attr_reader :result
+    attr_reader :node
 
     def run(node, feature_path, cucumber_configuration)
+      Celluloid.exception_handler do |ex|
+        p ex.message
+        node.release
+      end
+      @node = node
       node.lock
       randomNumber = SecureRandom.uuid
       deviceName = node.name
