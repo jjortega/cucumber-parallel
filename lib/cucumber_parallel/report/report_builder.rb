b4db2f0ebd149aa9c68d2d0323494dd5dcd35aeb
Juan Jose Ortega <jjortega@MacBook-Pro-de-Juan.local>
2016-11-24 11:11:24 +0100

    delete bybug

--- lib/cucumber_parallel/report/report_builder.rb
+++ lib/cucumber_parallel/report/report_builder.rb
@@ -36,7 +36,6 @@ module CucumberParallel
         config.compress_images = @options[:compress_images]
         config.additional_info = @options[:additional_info]
       end
-      byebug
       ReportBuilder.build_report
     end
 
